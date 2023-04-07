5923a58d8518c57f9645db1a0a11dc3e21f42832
Chidinma Ukazu <ukazuchidinma1@gmail.com>
2023-04-07 10:55:02 +0100

    made changes

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,13 @@
 'use strict';
 const btn = document.querySelector('.toggle_input');
 const themeText = document.querySelector('.dark');
-// Select the theme preference from localStorage
-const currentTheme = localStorage.getItem('theme');
+// Select the const currentTheme = localStorage.getItem('theme');
 // If the current theme in localStorage is "light"...
-if (currentTheme == 'light') {
-  // ...then use the .light-theme class
-  document.body.classList.add('light-theme');
-}
+// if (currentTheme == 'light') {
+//   // ...then use the .light-theme class
+//   document.body.classList.add('light-theme');
+// }
+
 // Listen for a click on the button
 btn.addEventListener('click', function () {
   // Toggle the .light-theme class on each click
@@ -19,12 +19,12 @@ btn.addEventListener('click', function () {
   }
 
   // Let's say the theme in the localstorage is equal to dark
-  let theme = 'dark';
-  // we den switch from dark to light, den we check if it changes,
-  if (document.body.classList.contains('light-theme')) {
-    // ...then let's make the theme light
-    theme = 'light';
-  }
-  // Then save the choice in localStorage
-  localStorage.setItem('theme', theme);
+  // let theme = 'dark';
+  // // we den switch from dark to light, den we check if it changes,
+  // if (document.body.classList.contains('light-theme')) {
+  //   // ...then let's make the theme light
+  //   theme = 'light';
+  // }
+  // // Then save the choice in localStorage
+  // localStorage.setItem('theme', theme);
 });
